388c62a9781229613965b1d21f3f748f7105a739
Daniel Mora <djosemora@ucundinamarca.edu.co>
2022-04-23 10:55:38 -0500

    Tipos de contrato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nomina';
+
+
+  title:string = "";
+  isSelect = false;
+  definition = "";
+
+  contrato(event:any) {
+    this.isSelect= true;
+    switch(event.value){
+      case '1': this.title = "Contrato a Término Fijo";
+      this.tipe1();
+      break;
+      case '2': this.title = "Contrato a término indefinido";
+      this.tipe2();
+      break;
+      case '3': this.title = "Contrato de Obra o labor";
+      this.tipe3();
+      break;
+      case '4': this.title = "Contrato civil por prestación de servicios";
+      this.tipe4();
+      break;
+      case '5': this.title = "Contrato de aprendizaje";
+      this.tipe5();
+      break;
+    }
+  }
+
+  tipe1(){
+    this.definition = "fffffff";
+  }
+  tipe2(){
+    
+  }
+  tipe3(){
+    
+  }
+  tipe4(){
+    
+  }
+  tipe5(){
+    
+  }
+
+
 }
